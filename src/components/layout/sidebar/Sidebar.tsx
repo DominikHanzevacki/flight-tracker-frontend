04f592202c6267e2d6fad17a3d31625e23bb261e
Dominik Hanževački <dodo.hanzevacki@gmail.com>
2025-04-05 22:51:49 +0200

    feature/Airports - Added airports and airports details page - Added GET, POST, PUT, DELETE apis for airports - added Google Maps api key

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import {Layout} from "antd";
 import {AppLogo} from "@components/logo/AppLogo";
 import {AppMenu} from "@components/layout/sidebar/menu/Menu";
-import {type ICollapsedProps} from "@/interfaces/sidebar/interface";
+import {type ICollapsedProps} from "@/interfaces/layout/sidebar/interface";
 
 const {Sider} = Layout;
 
--- src/pages/airports/header/AirportsHeader.tsx
+++ src/pages/airports/header/AirportsHeader.tsx
@@ -3,11 +3,11 @@ import {PlusOutlined} from "@ant-design/icons";
 import type {IHeaderProps} from "@/interfaces/layout/header/interface";
 
 
-export const AirlinesHeader = ({handleCreateModalOpen}: IHeaderProps) => {
+export const AirportsHeader = ({handleCreateModalOpen}: IHeaderProps) => {
   return (
     <Card className="border rounded-lg shadow-sm">
       <Flex justify="space-between" align="center">
-        <h2 className="text-center text-2xl font-bold">Airlines</h2>
+        <h2 className="text-center text-2xl font-bold">Airports</h2>
         <Button shape="circle"
           className="hover:!text-blue-light hover:!border-blue-light !text-blue-dark !border-blue-dark"
           onClick={handleCreateModalOpen}>
