14c88658fe949941eaec432a955b3099ba7a04c7
Dominik Hanževački <dodo.hanzevacki@gmail.com>
2025-04-05 16:08:13 +0200

    production - Removed proxy

--- vite.config.ts
+++ vite.config.ts
@@ -23,13 +23,6 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'https://flight-tracker-be-production.up.railway.app/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
   },
   build: {
     outDir: 'dist',
